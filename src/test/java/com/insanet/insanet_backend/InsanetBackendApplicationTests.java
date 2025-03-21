425325f09688aefc56ef5b62bcf9fc756aab1416
melisa-kabasakal <meliskabasakal33@gmail.com>
2025-03-21 21:28:27 +0300

    first commit

---
+++ src/test/java/com/insanet/insanet_backend/InsanetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insanet.insanet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsanetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
